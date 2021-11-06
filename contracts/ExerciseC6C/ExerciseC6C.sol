06fd7ade380d116c4edf4efa16791e82b2e3e0b5
Trebuchet Clement <clement.trebuchet.external@airbus.com>
2021-11-06 15:43:47 +0100

    End of the exercice 6c

--- contracts/ExerciseC6C/ExerciseC6C.sol
+++ contracts/ExerciseC6C/ExerciseC6C.sol
@@ -8,166 +8,142 @@ import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract ExerciseC6C {
-    using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
-
-    /********************************************************************************************/
-    /*                                       DATA VARIABLES                                     */
-    /********************************************************************************************/
-
-    struct Profile {
-        string id;
-        bool isRegistered;
-        bool isAdmin;
-        uint256 sales;
-        uint256 bonus;
-        address wallet;
-    }
-
-    address private contractOwner;              // Account used to deploy contract
-    mapping(string => Profile) employees;      // Mapping for storing employees
-
-    /********************************************************************************************/
-    /*                                       EVENT DEFINITIONS                                  */
-    /********************************************************************************************/
-
-    // No events
-
-    /**
-    * @dev Constructor
-    *      The deploying account becomes contractOwner
-    */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
-        contractOwner = msg.sender;
-    }
-
-    /********************************************************************************************/
-    /*                                       FUNCTION MODIFIERS                                 */
-    /********************************************************************************************/
-
-    // Modifiers help avoid duplication of code. They are typically used to validate something
-    // before a function is allowed to be executed.
-
-    /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner()
-    {
-        require(msg.sender == contractOwner, "Caller is not contract owner");
-        _;
-    }
-
-    /********************************************************************************************/
-    /*                                       UTILITY FUNCTIONS                                  */
-    /********************************************************************************************/
-
-   /**
-    * @dev Check if an employee is registered
-    *
-    * @return A bool that indicates if the employee is registered
-    */   
-    function isEmployeeRegistered
-                            (
-                                string id
-                            )
-                            external
-                            view
-                            returns(bool)
-    {
-        return employees[id].isRegistered;
-    }
-
-    /********************************************************************************************/
-    /*                                     SMART CONTRACT FUNCTIONS                             */
-    /********************************************************************************************/
-
-    function registerEmployee
-                                (
-                                    string id,
-                                    bool isAdmin,
-                                    address wallet
-                                )
-                                external
-                                requireContractOwner
-    {
-        require(!employees[id].isRegistered, "Employee is already registered.");
-
-        employees[id] = Profile({
-                                        id: id,
-                                        isRegistered: true,
-                                        isAdmin: isAdmin,
-                                        sales: 0,
-                                        bonus: 0,
-                                        wallet: wallet
-                                });
-    }
-
-    function getEmployeeBonus
-                            (
-                                string id
-                            )
-                            external
-                            view
-                            requireContractOwner
-                            returns(uint256)
-    {
-        return employees[id].bonus;
-    }
-
-    function updateEmployee
-                                (
-                                    string id,
-                                    uint256 sales,
-                                    uint256 bonus
-
-                                )
-                                internal
-                                requireContractOwner
-    {
-        require(employees[id].isRegistered, "Employee is not registered.");
-
-        employees[id].sales = employees[id].sales.add(sales);
-        employees[id].bonus = employees[id].bonus.add(bonus);
-
-    }
-
-    function calculateBonus
-                            (
-                                uint256 sales
-                            )
-                            internal
-                            view
-                            requireContractOwner
-                            returns(uint256)
-    {
-        if (sales < 100) {
-            return sales.mul(5).div(100);
-        }
-        else if (sales < 500) {
-            return sales.mul(7).div(100);
-        }
-        else {
-            return sales.mul(10).div(100);
-        }
-    }
-
-    function addSale
-                                (
-                                    string id,
-                                    uint256 amount
-                                )
-                                external
-                                requireContractOwner
-    {
-        updateEmployee(
-                        id,
-                        amount,
-                        calculateBonus(amount)
-        );
-    }
-
-
+	using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
+	
+	/********************************************************************************************/
+	/*                                       DATA VARIABLES                                     */
+	/********************************************************************************************/
+	
+	struct Profile {
+		string id;
+		bool isRegistered;
+		bool isAdmin;
+		uint256 sales;
+		uint256 bonus;
+		address wallet;
+	}
+	
+	address private contractOwner;              // Account used to deploy contract
+	mapping(string => Profile) employees;      // Mapping for storing employees
+	mapping(address => uint256) private authorizedContracts;
+	
+	/********************************************************************************************/
+	/*                                       EVENT DEFINITIONS                                  */
+	/********************************************************************************************/
+	
+	// No events
+	
+	/**
+	* @dev Constructor
+	*      The deploying account becomes contractOwner
+	*/
+	constructor
+	(
+	)
+	public
+	{
+		contractOwner = msg.sender;
+	}
+	
+	/********************************************************************************************/
+	/*                                       FUNCTION MODIFIERS                                 */
+	/********************************************************************************************/
+	
+	// Modifiers help avoid duplication of code. They are typically used to validate something
+	// before a function is allowed to be executed.
+	
+	/**
+	* @dev Modifier that requires the "ContractOwner" account to be the function caller
+	*/
+	modifier requireContractOwner()
+	{
+		require(msg.sender == contractOwner, "Caller is not contract owner");
+		_;
+	}
+	
+	modifier isCallerAuthorized() {
+		require(authorizedContracts[msg.sender] == 1, "The caller is not authorized");
+		_;
+	}
+	
+	/********************************************************************************************/
+	/*                                       UTILITY FUNCTIONS                                  */
+	/********************************************************************************************/
+	
+	/**
+	 * @dev Check if an employee is registered
+	 *
+	 * @return A bool that indicates if the employee is registered
+	 */
+	function isEmployeeRegistered
+	(
+		string id
+	)
+	external
+	view
+	returns (bool)
+	{
+		return employees[id].isRegistered;
+	}
+	
+	/********************************************************************************************/
+	/*                                     SMART CONTRACT FUNCTIONS                             */
+	/********************************************************************************************/
+	
+	function registerEmployee
+	(
+		string id,
+		bool isAdmin,
+		address wallet
+	)
+	external
+	requireContractOwner
+	{
+		require(!employees[id].isRegistered, "Employee is already registered.");
+		
+		employees[id] = Profile({
+		id : id,
+		isRegistered : true,
+		isAdmin : isAdmin,
+		sales : 0,
+		bonus : 0,
+		wallet : wallet
+		});
+	}
+	
+	function getEmployeeBonus
+	(
+		string id
+	)
+	external
+	view
+	requireContractOwner
+	returns (uint256)
+	{
+		return employees[id].bonus;
+	}
+	
+	function updateEmployee
+	(
+		string id,
+		uint256 sales,
+		uint256 bonus
+	
+	)
+	external
+	{
+		require(employees[id].isRegistered, "Employee is not registered.");
+		
+		employees[id].sales = employees[id].sales.add(sales);
+		employees[id].bonus = employees[id].bonus.add(bonus);
+		
+	}
+	
+	function authorizeContract(address _address) external requireContractOwner {
+		authorizedContracts[_address] = 1;
+	}
+	
+	function deauthorizeContract(address _address) external requireContractOwner {
+		delete authorizedContracts[_address];
+	}
 }
