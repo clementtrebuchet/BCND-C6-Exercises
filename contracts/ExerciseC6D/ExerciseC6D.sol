06af12c232b8f1d30a43b530bdcf63de50af594a
Trebuchet Clement <clement.trebuchet.external@airbus.com>
2021-11-06 18:03:54 +0100

    Add missing code in contracts/ExerciseC6D/ExerciseC6D.sol for the registerOracle() function

--- contracts/ExerciseC6D/ExerciseC6D.sol
+++ contracts/ExerciseC6D/ExerciseC6D.sol
@@ -92,13 +92,12 @@ contract ExerciseC6D {
                             payable
     {
         // CODE EXERCISE 1: Require registration fee
-        /* Enter code here */
+        require(msg.value >= REGISTRATION_FEE, "The registration fee should be greater or equal to the amount of 1 ETH");
 
         // CODE EXERCISE 1: Generate three random indexes (range 0-9) using generateIndexes for the calling oracle
-        /* Enter code here */
-
+        uint8[3] memory _randomIndexes = generateIndexes(msg.sender);
         // CODE EXERCISE 1: Assign the indexes to the oracle and save to the contract state
-        /* Enter code here */
+        oracles[msg.sender] = _randomIndexes;
     }
 
     function getOracle
